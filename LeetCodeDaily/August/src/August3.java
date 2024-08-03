c4453f31f6843e28b338e23291fdd89b0e11b99d
BILAL <bilalaliofficial1@gmail.com>
2024-08-03 22:15:57 +0530

    Added August 3 LC Solution

---
+++ LeetCodeDaily/August/src/August3.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class August3 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            map.put(target[i], map.getOrDefault(target[i], 0) - 1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) != 0) return false;
+        }
+        return true;
+    }
+}
